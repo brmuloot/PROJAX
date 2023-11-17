eb50cbcb4d3711cf6747fbe7925f3d3ef3179882
remiprince <remi.prince@michelin.com>
2023-11-17 14:49:37 +0100

    ajout logout + css

--- authentication/models.py
+++ authentication/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    CREATOR = 'CREATOR'
+    ADMIN = 'ADMIN'
     SUBSCRIBER = 'SUBSCRIBER'
   
     ROLE_CHOICES = (
-        (CREATOR, 'Creator'),
+        (ADMIN, 'Administrator'),
         (SUBSCRIBER, 'Subscriber'),
     )
     role = models.CharField(max_length=30, choices=ROLE_CHOICES)
--- mysite/urls.py
+++ mysite/urls.py
@@ -21,5 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path("blog/", include("blog.urls")),
     path("admin/", admin.site.urls),
-    #path("", include("authentication.urls"))
+    path("", include("authentication.urls"))
 ]
